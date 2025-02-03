15e7c89247762921aa8f40fb86b5efc3df52ebd5
Ro0628 <ronaldalajuan@gmail.com>
2025-02-02 23:29:00 -0600

    update

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/ro0628/ro0628.github.io.git";
+const repoURL = "https://github.com/ro0628/home";
 
 ghpages.publish(
   pathname,
